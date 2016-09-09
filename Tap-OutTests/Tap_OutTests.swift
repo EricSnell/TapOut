4349df512a1673f3c56a8b38b8262c98c4767125
Eric Snell <eric_snell21@yahoo.com>
2016-09-09 19:20:23 -0400

    Initial Commit

---
+++ Tap-OutTests/Tap_OutTests.swift
@@ -0,0 +1,36 @@
+//
+//  Tap_OutTests.swift
+//  Tap-OutTests
+//
+//  Created by Eric Snell on 9/9/16.
+//  Copyright © 2016 Eric Snell. All rights reserved.
+//
+
+import XCTest
+@testable import Tap_Out
+
+class Tap_OutTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
